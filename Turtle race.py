22c115f372897231e988cac6c38ed8ae441e7cb1
Frances Ugwu <ugwuuchennafrances@gmail.com>
2023-01-16 23:55:21 +0100

    Second commit

--- Turtle race.py
+++ Turtle race.py
@@ -1,38 +1,38 @@
 from turtle import Turtle, Screen
+import random
 
+is_race_on = False
 screen = Screen()
-
 screen.setup(width=500, height=400)
-user_input = screen.textinput(title="Make a bet", prompt="Who will win the race? pick a color ")
+user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter a color: ")
+colors = ["red", "orange", "yellow", "green", "blue", "purple"]
+y_positions = [-100, -60, -20, 20, 60, 100]
+all_turtles = []
 
-anna = Turtle(shape="turtle")
-anna.color("red")
-anna.penup()
-anna.goto(x=-230, y=-100)
+#Create 6 turtles
+for turtle_index in range(0, 6):
+    new_turtle = Turtle(shape="turtle")
+    new_turtle.penup()
+    new_turtle.color(colors[turtle_index])
+    new_turtle.goto(x=-230, y=y_positions[turtle_index])
+    all_turtles.append(new_turtle)
 
-bill = Turtle(shape="turtle")
-bill.color("blue")
-bill.penup()
-bill.goto(x=-230, y=-60)
+if user_bet:
+    is_race_on = True
 
-clay = Turtle(shape="turtle")
-clay.color("yellow")
-clay.penup()
-clay.goto(x=-230, y=-20)
+while is_race_on:
+    for turtle in all_turtles:
+        #230 is 250 - half the width of the turtle.
+        if turtle.xcor() > 230:
+            is_race_on = False
+            winning_color = turtle.pencolor()
+            if winning_color == user_bet:
+                print(f"You've won! The {winning_color} turtle is the winner!")
+            else:
+                print(f"You've lost! The {winning_color} turtle is the winner!")
 
-dell = Turtle(shape="turtle")
-dell.color("green")
-dell.penup()
-dell.goto(x=-230, y=20)
-
-wall = Turtle(shape="turtle")
-wall.color("orange")
-wall.penup()
-wall.goto(x=-230, y=60)
-
-tom = Turtle(shape="turtle")
-tom.color("purple")
-tom.penup()
-tom.goto(x=-230, y=100)
+        #Make each turtle move a random amount.
+        rand_distance = random.randint(0, 10)
+        turtle.forward(rand_distance)
 
 screen.exitonclick()
